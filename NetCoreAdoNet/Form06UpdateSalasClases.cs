541d5d81af56a5002521c521e2521eeae9e15599
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-14 11:13:50 +0100

    Implementación de clases para la lógica y conexión de la BBDD

---
+++ NetCoreAdoNet/Form06UpdateSalasClases.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using NetCoreAdoNet.Repositories;
+
+namespace NetCoreAdoNet
+{
+    public partial class Form06UpdateSalasClases : Form
+    {
+        RepositorySalas repo;
+        public Form06UpdateSalasClases()
+        {
+            InitializeComponent();
+            this.repo = new RepositorySalas();
+            this.loadSalas();
+        }
+
+        private void loadSalas()
+        {
+            List<string> salas = this.repo.GetNombresSalas();
+            this.lstSalas.Items.Clear();
+            foreach(string name in salas)
+            {
+                this.lstSalas.Items.Add(name);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string newName = this.txtNombre.Text;
+            string oldName = this.lstSalas.SelectedItem.ToString();
+            int registros = this.repo.updateSala(newName, oldName);
+            MessageBox.Show("Salas modificadas: " + registros);
+            this.loadSalas();
+        }
+    }
+}
---
+++ NetCoreAdoNet/Repositories/RepositorySalas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace NetCoreAdoNet.Repositories
+{
+    public class RepositorySalas
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public RepositorySalas()
+        {
+            string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+        }
+
+        public List<string> GetNombresSalas()
+        {
+            string sql = "select distinct NOMBRE from SALA";
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            List<string> salas = new List<string>();
+            while (this.reader.Read())
+            {
+                string nombre = this.reader["NOMBRE"].ToString();
+                salas.Add(nombre);
+            }
+            this.reader.Close();
+            this.cn.Close();
+            return salas;
+        }
+
+        public int updateSala(string newName, string oldName)
+        {
+            string sql = "update SALA set NOMBRE=@newname where NOMBRE=@oldname";
+            SqlParameter pamNew = new SqlParameter("@newname", newName);
+            SqlParameter pamOld = new SqlParameter("@oldname", oldName);
+            this.com.Parameters.Add(pamNew);
+            this.com.Parameters.Add(pamOld);
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            int registros = this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            return registros;
+        }
+
+    }
+}
