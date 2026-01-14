b768004ad11f27c8e24940b574562e67c1c11f43
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-14 13:21:08 +0100

    Form departamentos empleados finalizado

---
+++ NetCoreAdoNet/Form07DepartamentosEmpleados.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetCoreAdoNet.Repositories;
+
+namespace NetCoreAdoNet
+{
+    public partial class Form07DepartamentosEmpleados : Form
+    {
+        RepositoryDepartamentosEmpleados repo;
+        public Form07DepartamentosEmpleados()
+        {
+            InitializeComponent();
+            this.repo = new RepositoryDepartamentosEmpleados();
+            this.loadDepartamentos();
+        }
+
+        private async void loadDepartamentos()
+        {
+            List<string> departamentos = await this.repo.GetNombresDepartamentosAsync();
+            this.lstDepartamentos.Items.Clear();
+            foreach (string name in departamentos)
+            {
+                this.lstDepartamentos.Items.Add(name);
+            }
+        }
+
+        private async void loadEmpleados()
+        {
+            string dnombre = this.lstDepartamentos.SelectedItem.ToString();
+            this.lstEmpleados.Items.Clear();
+            List<string> empleadosDepartamentos = await this.repo.GetApellidosEmpleadosPorDepartamentoAsync(dnombre);
+            foreach (string name in empleadosDepartamentos)
+            {
+                this.lstEmpleados.Items.Add(name);
+            }
+        }
+
+
+        private async void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string apellido = this.lstEmpleados.SelectedItem.ToString();
+            int registro = await this.repo.GetApellidoEmpleadoAsync(apellido);
+            MessageBox.Show("Empleados eliminados: " + registro);
+            this.loadEmpleados();
+
+        }
+
+        private async void lstDepartamentos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string dnombre = this.lstDepartamentos.SelectedItem.ToString();
+            this.lstEmpleados.Items.Clear();
+            List<string> empleadosDepartamentos = await this.repo.GetApellidosEmpleadosPorDepartamentoAsync(dnombre);
+            foreach(string name in empleadosDepartamentos)
+            {
+                this.lstEmpleados.Items.Add(name);
+            }
+        }
+    }
+}
---
+++ NetCoreAdoNet/Repositories/RepositoryDepartamentosEmpleados.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace NetCoreAdoNet.Repositories
+{
+    public class RepositoryDepartamentosEmpleados
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public RepositoryDepartamentosEmpleados()
+        {
+            string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+        }
+
+        public async Task<List<string>> GetNombresDepartamentosAsync()
+        {
+            string sql = "select distinct DNOMBRE from DEPARTAMENTOS";
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            List<string> departamentos = new List<string>();
+            while(await this.reader.ReadAsync())
+            {
+                string nombre = this.reader["DNOMBRE"].ToString();
+                departamentos.Add(nombre);
+            }
+            await this.cn.CloseAsync();
+            await this.reader.CloseAsync();
+            return departamentos;
+        }
+
+        public async Task<List<string>> GetApellidosEmpleadosPorDepartamentoAsync(string dnombre)
+        {
+            string sql = "select APELLIDO from EMP where DEPT_NO=(select DEPT_NO from DEPT where DNOMBRE=@dnombre)";
+            SqlParameter pamNombre = new SqlParameter("@dnombre", dnombre);
+            this.com.Parameters.Add(pamNombre);
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            List<string> empleados = new List<string>();
+            while(await this.reader.ReadAsync())
+            {
+                string apellido = this.reader["APELLIDO"].ToString();
+                empleados.Add(apellido);
+            }
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+            return empleados;
+        }
+
+        public async Task<int> GetApellidoEmpleadoAsync(string apellido)
+        {
+            string sql = "delete from EMP where APELLIDO=@apellido";
+            SqlParameter pamApellido = new SqlParameter("@apellido", apellido);
+            this.com.Parameters.Add(pamApellido);
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            int registros = await this.com.ExecuteNonQueryAsync();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+            return registros;
+        }
+
+    }
+}
