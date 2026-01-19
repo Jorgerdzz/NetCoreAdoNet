621a69c966eab51e82c81d0beb77cbb6ae917717
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-19 14:22:50 +0100

    Form Parametros salida finalizado

---
+++ NetCoreAdoNet/Form13ParametrosSalida.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace NetCoreAdoNet
+{
+    partial class Form13ParametrosSalida
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            cmbDepartamentos = new ComboBox();
+            label2 = new Label();
+            lstEmpleados = new ListBox();
+            btnMostrarDatos = new Button();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(52, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Departamentos";
+            // 
+            // cmbDepartamentos
+            // 
+            cmbDepartamentos.FormattingEnabled = true;
+            cmbDepartamentos.Location = new Point(52, 68);
+            cmbDepartamentos.Name = "cmbDepartamentos";
+            cmbDepartamentos.Size = new Size(121, 23);
+            cmbDepartamentos.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(247, 34);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Empleados";
+            // 
+            // lstEmpleados
+            // 
+            lstEmpleados.FormattingEnabled = true;
+            lstEmpleados.Location = new Point(252, 65);
+            lstEmpleados.Name = "lstEmpleados";
+            lstEmpleados.Size = new Size(198, 304);
+            lstEmpleados.TabIndex = 3;
+            // 
+            // btnMostrarDatos
+            // 
+            btnMostrarDatos.Location = new Point(52, 119);
+            btnMostrarDatos.Name = "btnMostrarDatos";
+            btnMostrarDatos.Size = new Size(121, 30);
+            btnMostrarDatos.TabIndex = 4;
+            btnMostrarDatos.Text = "Mostrar datos";
+            btnMostrarDatos.UseVisualStyleBackColor = true;
+            btnMostrarDatos.Click += btnMostrarDatos_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(52, 189);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Suma salarial";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(52, 207);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(121, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(52, 250);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Media salarial";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(52, 268);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(121, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(52, 318);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Personas";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(52, 346);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(121, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // Form13ParametrosSalida
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(btnMostrarDatos);
+            Controls.Add(lstEmpleados);
+            Controls.Add(label2);
+            Controls.Add(cmbDepartamentos);
+            Controls.Add(label1);
+            Name = "Form13ParametrosSalida";
+            Text = "Form13ParametrosSalida";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cmbDepartamentos;
+        private Label label2;
+        private ListBox lstEmpleados;
+        private Button btnMostrarDatos;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox3;
+    }
+}
---
+++ NetCoreAdoNet/Form13ParametrosSalida.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#region
+//create procedure SP_EMPLEADOS_DEPARTAMENTOS_OUT
+//(@nombre nvarchar(50),
+//    @suma int OUT,
+//    @media int OUT,
+//    @personas int OUT)
+//as
+//	declare @iddept int
+//	select @iddept = DEPT_NO from DEPT where DNOMBRE = @nombre
+//	--LA CONSULTA DLE PROCEDIMIENTO
+//	select * from EMP where DEPT_NO = @iddept
+//	--Rellenamos las variables de salida
+//	select @suma = SUM(SALARIO), @media = AVG(SALARIO), @personas = COUNT(EMP_NO) from EMP
+//	where DEPT_NO = @iddept
+//go
+#endregion
+
+namespace NetCoreAdoNet
+{
+    public partial class Form13ParametrosSalida : Form
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+        public Form13ParametrosSalida()
+        {
+            InitializeComponent();
+            string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+            this.LoadDepartamentos();
+        }
+
+        private async Task LoadDepartamentos()
+        {
+            string sql = "SP_ALL_DEPARTAMENTOS";
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            this.cmbDepartamentos.Items.Clear();
+            while (await this.reader.ReadAsync())
+            {
+                string nombre = this.reader["DNOMBRE"].ToString();
+                this.cmbDepartamentos.Items.Add(nombre);
+            }
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+        }
+
+        private async void btnMostrarDatos_Click(object sender, EventArgs e)
+        {
+            string sql = "SP_EMPLEADOS_DEPARTAMENTOS_OUT";
+            string nombre = this.cmbDepartamentos.SelectedItem.ToString();
+            this.com.Parameters.AddWithValue("@nombre", nombre);
+            //LOS PARAMETROS DE SALIDA DEBEMOS CREARLOS DE FORMA EXPLÍCITA
+            SqlParameter pamSuma = new SqlParameter();
+            pamSuma.ParameterName = "@suma";
+            pamSuma.Value = 0;
+            pamSuma.Direction = ParameterDirection.Output;
+            this.com.Parameters.Add(pamSuma);
+            SqlParameter pamMedia = new SqlParameter();
+            pamMedia.ParameterName = "@media";
+            pamMedia.Value = 0;
+            pamMedia.Direction = ParameterDirection.Output;
+            this.com.Parameters.Add(pamMedia);
+            SqlParameter pamPersonas = new SqlParameter();
+            pamPersonas.ParameterName = "@personas";
+            pamPersonas.Value = 0;
+            pamPersonas.Direction = ParameterDirection.Output;
+            this.com.Parameters.Add(pamPersonas);
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            this.lstEmpleados.Items.Clear();
+            while (await this.reader.ReadAsync())
+            {
+                string apellido = this.reader["APELLIDO"].ToString();
+                this.lstEmpleados.Items.Add(apellido);
+            }
+            await this.reader.CloseAsync();
+            this.textBox1.Text = pamSuma.Value.ToString();
+            this.textBox2.Text = pamMedia.Value.ToString();
+            this.textBox3.Text = pamPersonas.Value.ToString();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+        }
+    }
+}
