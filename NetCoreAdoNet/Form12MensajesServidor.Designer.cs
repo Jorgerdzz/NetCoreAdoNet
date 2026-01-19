7f5fd48fdb1b4297ed8350d2980e52a620185d1a
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-19 13:01:48 +0100

    Form mensajes servidor finalizado

---
+++ NetCoreAdoNet/Form12MensajesServidor.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace NetCoreAdoNet
+{
+    partial class Form12MensajesServidor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtId = new TextBox();
+            label2 = new Label();
+            txtNombre = new TextBox();
+            label3 = new Label();
+            txtLocalidad = new TextBox();
+            btnNuevoDepartamento = new Button();
+            label4 = new Label();
+            lstDepartamentos = new ListBox();
+            lblServidor = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(55, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Id";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(55, 69);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(126, 23);
+            txtId.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(55, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(55, 141);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(126, 23);
+            txtNombre.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(55, 193);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Localidad";
+            // 
+            // txtLocalidad
+            // 
+            txtLocalidad.Location = new Point(55, 225);
+            txtLocalidad.Name = "txtLocalidad";
+            txtLocalidad.Size = new Size(126, 23);
+            txtLocalidad.TabIndex = 5;
+            // 
+            // btnNuevoDepartamento
+            // 
+            btnNuevoDepartamento.Location = new Point(55, 269);
+            btnNuevoDepartamento.Name = "btnNuevoDepartamento";
+            btnNuevoDepartamento.Size = new Size(126, 43);
+            btnNuevoDepartamento.TabIndex = 6;
+            btnNuevoDepartamento.Text = "Nuevo departamento";
+            btnNuevoDepartamento.UseVisualStyleBackColor = true;
+            btnNuevoDepartamento.Click += btnNuevoDepartamento_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(252, 36);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Departamentos";
+            // 
+            // lstDepartamentos
+            // 
+            lstDepartamentos.FormattingEnabled = true;
+            lstDepartamentos.Location = new Point(256, 69);
+            lstDepartamentos.Name = "lstDepartamentos";
+            lstDepartamentos.Size = new Size(414, 259);
+            lstDepartamentos.TabIndex = 8;
+            // 
+            // lblServidor
+            // 
+            lblServidor.AutoSize = true;
+            lblServidor.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblServidor.ForeColor = SystemColors.HotTrack;
+            lblServidor.Location = new Point(67, 347);
+            lblServidor.Name = "lblServidor";
+            lblServidor.Size = new Size(95, 21);
+            lblServidor.TabIndex = 9;
+            lblServidor.Text = "lblServidor";
+            // 
+            // Form12MensajesServidor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblServidor);
+            Controls.Add(lstDepartamentos);
+            Controls.Add(label4);
+            Controls.Add(btnNuevoDepartamento);
+            Controls.Add(txtLocalidad);
+            Controls.Add(label3);
+            Controls.Add(txtNombre);
+            Controls.Add(label2);
+            Controls.Add(txtId);
+            Controls.Add(label1);
+            Name = "Form12MensajesServidor";
+            Text = "Form12MensajesServidor";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtId;
+        private Label label2;
+        private TextBox txtNombre;
+        private Label label3;
+        private TextBox txtLocalidad;
+        private Button btnNuevoDepartamento;
+        private Label label4;
+        private ListBox lstDepartamentos;
+        private Label lblServidor;
+    }
+}
---
+++ NetCoreAdoNet/Form12MensajesServidor.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#region
+//create procedure SP_ALL_DEPARTAMENTOS
+//as
+//	select * from DEPT
+//go
+
+//alter procedure SP_INSERT_DEPARTAMENTO
+//(@numero int, @nombre nvarchar(50), @localidad nvarchar(50))
+//as
+//	if(UPPER(@localidad) = 'TERUEL')
+//	begin
+//		print 'TERUEL NO EXISTE'
+//	end
+//	else
+//	begin
+//		insert into DEPT values(@numero, @nombre, @localidad)
+//	end
+//go
+#endregion
+
+namespace NetCoreAdoNet
+{
+    public partial class Form12MensajesServidor : Form
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+        public Form12MensajesServidor()
+        {
+            InitializeComponent();
+            string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            //string connectionString = @"Data Source=sqlpaco3430.database.windows.net;Initial Catalog=AZURETAJAMAR;Persist Security Info=True;User ID=adminsql;Password=Admin123;Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            //AGREGAMOS EL EVENTO PARA CAPTURAR MENSAJES
+            this.cn.InfoMessage += Cn_InfoMessage;
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+            this.LoadDepartamentos();
+        }
+
+        private async void Cn_InfoMessage(object sender, SqlInfoMessageEventArgs e)
+        {
+            this.lblServidor.Text = e.Message;
+        }
+
+        private async void LoadDepartamentos()
+        {
+            string sql = "SP_ALL_DEPARTAMENTOS";
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            this.reader = await this.com.ExecuteReaderAsync();
+            this.lstDepartamentos.Items.Clear();
+            while (await this.reader.ReadAsync())
+            {
+                string deptno = this.reader["DEPT_NO"].ToString();
+                string nombre = this.reader["DNOMBRE"].ToString();
+                string localidad = this.reader["LOC"].ToString();
+                this.lstDepartamentos.Items.Add(deptno + " - " + nombre + " - " + localidad);
+            }
+            await this.reader.CloseAsync();
+            await this.cn.CloseAsync();
+        }
+
+        private async void btnNuevoDepartamento_Click(object sender, EventArgs e)
+        {
+            this.lblServidor.Text = "";
+            string sql = "SP_INSERT_DEPARTAMENTO";
+            int deptno = int.Parse(this.txtId.Text);
+            string nombre = this.txtNombre.Text;
+            string localidad = this.txtLocalidad.Text;
+            this.com.Parameters.AddWithValue("@numero", deptno);
+            this.com.Parameters.AddWithValue("@nombre", nombre);
+            this.com.Parameters.AddWithValue("@localidad", localidad);
+            this.com.CommandType = CommandType.StoredProcedure;
+            this.com.CommandText = sql;
+            await this.cn.OpenAsync();
+            //int registros = await this.com.ExecuteNonQueryAsync();
+            int registros = this.com.ExecuteNonQuery();
+            await this.cn.CloseAsync();
+            this.com.Parameters.Clear();
+            MessageBox.Show("Departamentos insertados: " + registros);
+            this.LoadDepartamentos();
+        }
+    }
+}
